e69e523753eb59fc22e01f66ce197c2a734fa28d
linuslabo <linuslabo@gmail.com>
2018-11-12 18:44:12 +1000

    added support for fractions of hour

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ var nextWeekStart = moment().utc().add(1, 'weeks').isoWeekday(1).startOf('day');
 var localTime = nextWeekStart.local();
 
 document.getElementById("weekStartDayLocal").innerHTML = localTime.format('dddd');
-document.getElementById("weekStartTimeLocal").innerHTML = localTime.format('h a');
+document.getElementById("weekStartTimeLocal").innerHTML =
+    localTime.minute() ? localTime.format('h:mm a') : localTime.format('h a');
 document.getElementById("timeZoneName").innerHTML = moment.tz.guess()
